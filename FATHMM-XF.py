40408757d1bc59b5fb7f7c9d2ead4163574cbe0b
luisa gracia <luisagmazuca@gmail.com>
2022-10-27 14:43:54 -0600

    update

--- FATHMM-XF.py
+++ FATHMM-XF.py
@@ -5,8 +5,9 @@ Created on Sun Oct 23 23:14:47 2022
 @author: luisa
 """
 import csv
-#FATHMM-XF inout generator
-#1. Create a comma delimited text document containing the following
+import pandas as pd
+#FATHMM-XF input generator
+#1. Create a comma delimited text document containing the following (to use as input) #step 5
 chrom_num = [] #store chrom num
 pos= [] #store position of variant
 ref_base = [] #store ref base
@@ -26,14 +27,13 @@ num = [] # for each chrom store only the number
 for i in range(len(chrom_num)):
     num.append(chrom_num[i][3])
 
-#the word "chromosome" in the first col
-
-#strand -> "1" for all since all alleles in the database are reported in the forward orientation
+#using the lists, create the comma delimited file #step 6
 with open('C:\\Users\\luisa\\Documents\\Fall 2022\\Post Gen Analysis\\Homework 4\\output_for_fathmm.txt', 'w') as f:
     for i in range(len(num)):
         f.write(num[i] + "," + pos[i] + "," + ref_base[i] + "," + alt_base[i] + "\n")
 
-#Merge output with OMI file
+#Merge output with OMI file #step 13
+#create lists with needed information
 coding_score = []
 non_coding_score = []
 warning_msg = []
@@ -45,14 +45,23 @@ with open('C:\\Users\\luisa\\Documents\\Fall 2022\\Post Gen Analysis\\Homework 4
             coding_score.append(row[4])
             non_coding_score.append(row[5])
             warning_msg.append(row[6])
-        else:
+        else: #create col headers
             coding_score.append("coding score")
             non_coding_score.append("non coding score")
             warning_msg.append("warning")
-        line_count+=1
-        
-        
-#add new cols
+        line_count+=1       
+
+#add cols to omi using pandas df #step 14
+omi_file_df = pd.read_csv('C:\\Users\\luisa\\Documents\\Fall 2022\\Post Gen Analysis\\Homework 4\\merged_omi.csv') #CHANGE NAME TO FILE
+omi_file_df["coding score"] = coding_score
+omi_file_df['non coding score'] = non_coding_score
+omi_file_df['warning'] = warning_msg
+
+#convert df to csv #step 15
+omi_file_df.to_csv('C:\\Users\\luisa\\Documents\\Fall 2022\\Post Gen Analysis\\Homework 4\\pre_final_omi.csv', index=False) #CHANGE NAME TO FILE
+
+"""
+#add new cols other method DO NOT USE
 from csv import writer
 from csv import reader
 def add_column_in_csv(input_file, output_file, transform_row):
@@ -66,4 +75,5 @@ def add_column_in_csv(input_file, output_file, transform_row):
 
 add_column_in_csv('C:\\Users\\luisa\\Documents\\Fall 2022\\Post Gen Analysis\\Homework 4\\merged_omi.csv', 'C:\\Users\\luisa\\Documents\\Fall 2022\\Post Gen Analysis\\Homework 4\\merged_omi1.csv', lambda row, line_num: row.append(coding_score[line_num - 1]))  
 add_column_in_csv('C:\\Users\\luisa\\Documents\\Fall 2022\\Post Gen Analysis\\Homework 4\\merged_omi1.csv', 'C:\\Users\\luisa\\Documents\\Fall 2022\\Post Gen Analysis\\Homework 4\\merged_omi2.csv', lambda row, line_num: row.append(non_coding_score[line_num - 1]))   
-add_column_in_csv('C:\\Users\\luisa\\Documents\\Fall 2022\\Post Gen Analysis\\Homework 4\\merged_omi2.csv', 'C:\\Users\\luisa\\Documents\\Fall 2022\\Post Gen Analysis\\Homework 4\\merged_omi3.csv', lambda row, line_num: row.append(warning_msg[line_num - 1]))         
+add_column_in_csv('C:\\Users\\luisa\\Documents\\Fall 2022\\Post Gen Analysis\\Homework 4\\merged_omi2.csv', 'C:\\Users\\luisa\\Documents\\Fall 2022\\Post Gen Analysis\\Homework 4\\merged_omi3.csv', lambda row, line_num: row.append(warning_msg[line_num - 1]))
+"""
--- SNP_Nexus.py
+++ SNP_Nexus.py
@@ -9,17 +9,17 @@ import pandas as pd
 import os
 import csv
 #1. Combine all OMI files into 1
-#get all files
+#get all files #step 1
 path = "C:\\Users\\luisa\\Documents\\Fall 2022\\Post Gen Analysis\\Homework 4\\files"
 file_list = os.listdir(path)
 df_append = pd.DataFrame()
 for file in file_list:
             df_temp = pd.read_csv(file)
             df_append = df_append.append(df_temp, ignore_index=True)
-#export
+#export #step 2
 df_append.to_csv('merged_omi.csv', index=False)
 
-#2. Create a tab delimited text document containing the following
+#2. Create a tab delimited text document containing the following (to use as input) #step 3
 chrom_num = [] #store chrom num
 pos_omi= [] #store position of variant
 ref_base = [] #store ref base
@@ -35,18 +35,19 @@ with open('C:\\Users\\luisa\\Documents\\Fall 2022\\Post Gen Analysis\\Homework 4
             alt_base.append(row[6])
         line_count+=1
 
-num = [] # for each chrom store only the number
+num = [] # for each chrom store only the number of the chrom
 for i in range(len(chrom_num)):
     num.append(chrom_num[i][3])
 
-#the word "chromosome" in the first col
-
-#strand -> "1" for all since all alleles in the database are reported in the forward orientation
+#using the lists, create the tab delimited file #step 4
+#put the word "chromosome" in the first col 
+#in the last col strand -> "1" for all since all alleles in the database are reported in the forward orientation
 with open('C:\\Users\\luisa\\Documents\\Fall 2022\\Post Gen Analysis\\Homework 4\\output_for_snpnexus.txt', 'w') as f:
     for i in range(len(num)):
         f.write("chromosome" + "\t" + num[i] + "\t" + pos_omi[i] + "\t" + ref_base[i] + "\t" + alt_base[i] + "\t" + "1" + "\n")
 
-#Merge output with OMI file
+#After running snpnexus, merge output with OMI file #step 7
+#from the gene coords file get dbsnp nexus col and its corresponding position
 dbsnp = []
 pos_snpnexus = []
 with open('C:\\Users\\luisa\\Documents\\Fall 2022\\Post Gen Analysis\\Homework 4\\snp_nexus_outpu\\gen_coords_HW4.csv') as csv_file:
@@ -57,7 +58,8 @@ with open('C:\\Users\\luisa\\Documents\\Fall 2022\\Post Gen Analysis\\Homework 4
             dbsnp.append(row[1])
             pos_snpnexus.append(row[3])
         line_count+=1
-            
+
+#reorder dbsn since the output from snpnexus is in ascending order #step 8      
 reordered_dbsnp = []
 for i in range(len(pos_omi)):
     for j in range(len(pos_snpnexus)):
@@ -66,7 +68,8 @@ for i in range(len(pos_omi)):
             break
         elif j == len(pos_snpnexus)-1 and pos_snpnexus[j] != pos_omi[i]:
             reordered_dbsnp.append("N/A")
-        
+
+#from the near genes file get overlapped gene, annotation and their respective position #step 9 
 pos_near_snpnexus = []            
 overlapped_gene = []
 annotation = []
@@ -80,6 +83,7 @@ with open('C:\\Users\\luisa\\Documents\\Fall 2022\\Post Gen Analysis\\Homework 4
             annotation.append(row[5])
         line_count+=1
 
+#reorder overlapped gene and annotation #step 10
 reordered_overlapped_gene = []
 reordered_annotation = []
 for i in range(len(pos_omi)):
@@ -92,7 +96,11 @@ for i in range(len(pos_omi)):
             reordered_overlapped_gene.append("N/A")
             reordered_annotation.append("N/A")
             
-#Add lists to csv to complete omi file
+#Add lists to csv to complete omi file #step 11
+omi_file_df = pd.read_csv('C:\\Users\\luisa\\Documents\\Fall 2022\\Post Gen Analysis\\Homework 4\\merged_omi3.csv') #CHANGE NAME TO FILE
+omi_file_df['dbsnp'] = reordered_dbsnp
+omi_file_df['overlapped gene'] = reordered_overlapped_gene
+omi_file_df['annotation'] = reordered_annotation
 
-
-
+#convert df to csv #step 12
+omi_file_df.to_csv('C:\\Users\\luisa\\Documents\\Fall 2022\\Post Gen Analysis\\Homework 4\\final_omi.csv', index=False) #CHANGE NAME TO FILE
